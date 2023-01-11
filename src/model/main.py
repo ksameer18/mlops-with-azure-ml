9e6f7e31a45f429c3b557fe421e37312dc6437a2
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-01-11 16:04:09 +0530

    Update main.py

--- src/model/main.py
+++ src/model/main.py
@@ -8,7 +8,7 @@ import mlflow
 
 # define functions
 def main(args):
-    
+
     # enable auto logging
     mlflow.autolog()
 
@@ -25,7 +25,8 @@ def main(args):
     train_model(X_train, X_test, y_train, y_test)
 
 def get_csvs_df(path):
-    
+
+
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
@@ -34,25 +35,27 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 def cleanse_data(df):
-    
-    taxi_df = df[['vendorID','passengerCount', 'tripDistance', 
-                  'pickupLongitude', 'pickupLatitude', 
+
+
+    taxi_df = df[['vendorID', 'passengerCount', 'tripDistance',
+                  'pickupLongitude', 'pickupLatitude',
                   'dropoffLongitude', 'dropoffLatitude', 'totalAmount']]
     final_df = taxi_df.query("pickupLatitude>=40.53 and pickupLatitude<=40.88")
     final_df = final_df.query(
         "pickupLongitude>=-74.09 and pickupLongitude<=-73.72")
     final_df = final_df.query("tripDistance>=0.25 and tripDistance<31")
     final_df = final_df.query("passengerCount>0 and totalAmount>0")
-    columns_to_remove_for_training = ["pickupLongitude", 
-                                      "pickupLatitude", 
-                                      "dropoffLongitude", 
+    columns_to_remove_for_training = ["pickupLongitude",
+                                      "pickupLatitude",
+                                      "dropoffLongitude",
                                       "dropoffLatitude"]
     for col in columns_to_remove_for_training:
         final_df.pop(col)
     return final_df
 
 def split_data(final_df):
-    
+
+
     X = final_df.drop(["totalAmount"], axis=1)
     y = final_df["totalAmount"]
     X_train, X_test, y_train, y_test = train_test_split(
@@ -60,12 +63,14 @@ def split_data(final_df):
     return X_train, X_test, y_train, y_test
 
 def train_model(X_train, X_test, y_train, y_test):
-    
+
+
     model = LinearRegression()
     model.fit(X_train, y_train)
 
 def parse_args():
-    
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--training_data", dest='training_data',
                         type=str)
@@ -74,8 +79,7 @@ def parse_args():
 
 if __name__ == "__main__":
 
-    
-    # add space in logs
+
     print("\n\n")
     print("*" * 60)
 
