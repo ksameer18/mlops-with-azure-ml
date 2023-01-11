68b18f80c6ac6256eb73f866dd9ba42c4402b9fb
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-01-11 15:58:30 +0530

    Update main.py

--- src/model/main.py
+++ src/model/main.py
@@ -8,6 +8,7 @@ import mlflow
 
 # define functions
 def main(args):
+    
     # enable auto logging
     mlflow.autolog()
 
@@ -21,38 +22,50 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(final_df)
 
     # train model
-    model = train_model(X_train, X_test, y_train, y_test)
+    train_model(X_train, X_test, y_train, y_test)
 
 def get_csvs_df(path):
+    
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)   
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 def cleanse_data(df):
-    taxi_df = df[['vendorID','passengerCount','tripDistance','pickupLongitude','pickupLatitude','dropoffLongitude','dropoffLatitude','totalAmount']]
+    
+    taxi_df = df[['vendorID','passengerCount', 'tripDistance', 
+                  'pickupLongitude', 'pickupLatitude', 
+                  'dropoffLongitude', 'dropoffLatitude', 'totalAmount']]
     final_df = taxi_df.query("pickupLatitude>=40.53 and pickupLatitude<=40.88")
-    final_df = final_df.query("pickupLongitude>=-74.09 and pickupLongitude<=-73.72")
+    final_df = final_df.query(
+        "pickupLongitude>=-74.09 and pickupLongitude<=-73.72")
     final_df = final_df.query("tripDistance>=0.25 and tripDistance<31")
     final_df = final_df.query("passengerCount>0 and totalAmount>0")
-    columns_to_remove_for_training = ["pickupLongitude", "pickupLatitude", "dropoffLongitude", "dropoffLatitude"]
+    columns_to_remove_for_training = ["pickupLongitude", 
+                                      "pickupLatitude", 
+                                      "dropoffLongitude", 
+                                      "dropoffLatitude"]
     for col in columns_to_remove_for_training:
         final_df.pop(col)
     return final_df
 
 def split_data(final_df):
+    
     X = final_df.drop(["totalAmount"], axis=1)
     y = final_df["totalAmount"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=223)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=223)
     return X_train, X_test, y_train, y_test
 
 def train_model(X_train, X_test, y_train, y_test):
-    model=LinearRegression()
+    
+    model = LinearRegression()
     model.fit(X_train, y_train)
 
 def parse_args():
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--training_data", dest='training_data',
                         type=str)
@@ -61,6 +74,7 @@ def parse_args():
 
 if __name__ == "__main__":
 
+    
     # add space in logs
     print("\n\n")
     print("*" * 60)
