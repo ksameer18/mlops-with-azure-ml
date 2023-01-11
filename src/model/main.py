f5aeba9415483d6a07d5470de672c3b7f00dea64
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-01-11 16:08:29 +0530

    Update main.py

--- src/model/main.py
+++ src/model/main.py
@@ -24,9 +24,8 @@ def main(args):
     # train model
     train_model(X_train, X_test, y_train, y_test)
 
+
 def get_csvs_df(path):
-
-
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
@@ -34,9 +33,8 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+
 def cleanse_data(df):
-
-
     taxi_df = df[['vendorID', 'passengerCount', 'tripDistance',
                   'pickupLongitude', 'pickupLatitude',
                   'dropoffLongitude', 'dropoffLatitude', 'totalAmount']]
@@ -53,33 +51,29 @@ def cleanse_data(df):
         final_df.pop(col)
     return final_df
 
+
 def split_data(final_df):
-
-
     X = final_df.drop(["totalAmount"], axis=1)
     y = final_df["totalAmount"]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=223)
     return X_train, X_test, y_train, y_test
 
+
 def train_model(X_train, X_test, y_train, y_test):
-
-
     model = LinearRegression()
     model.fit(X_train, y_train)
 
+
 def parse_args():
-
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--training_data", dest='training_data',
                         type=str)
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
-
-
     print("\n\n")
     print("*" * 60)
 
